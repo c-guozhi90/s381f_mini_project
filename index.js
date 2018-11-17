474421f5512a580bf241596d95b6056311103556
c-guozhi90 <c.guozhi90@gmail.com>
2018-11-17 14:30:13 +0800

    add common function - error routing and template

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const Express = require('express')
 const app = Express()
 const HomepageHandle = require('./page_handles/sample_homepage')
+global.redirectionError = {
+    status,
+    title: '',
+    contents: ''
+}
 
 app.set('view engine', 'ejs')
-app.use('/css',Express.static('css'))
+app.use('/css', Express.static('css'))
 
 // set routes
 app.get('/', function (req, res) {
-    HomepageHandle.homepage(req,res)
+    HomepageHandle.homepage(req, res)
 })
 
+app.get('/error', function (req, res) {
+    res.status(status)
+    res.render('redirection_error_template')
+})
 app.listen(process.env.PORT || 8099)
