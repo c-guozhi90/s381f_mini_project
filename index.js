0c0031bf8e236ca0f817902aec16ac82439c2c1b
Raven Chan <c.guozhi90@gmail.com>
2018-11-22 19:40:36 +0800

    hotfix - fix bugs in error handling

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const Express = require('express')
 const app = Express()
+const session = require('cookie-session')
 const HomepageHandle = require('./page_handles/sample_homepage')
+
 global.redirectionError = {
-    status,
-    title: '',
-    contents: ''
+    status: 404,
+    title: '404 not found!',
+    contents: 'The page you requested does not exist!'
 }
 
 app.set('view engine', 'ejs')
 app.use('/css', Express.static('css'))
-
+app.use(session({
+    user_name: '',
+    keys: ['secret keys']
+}))
 // set routes
 app.get('/', function (req, res) {
     HomepageHandle.homepage(req, res)
 })
 
 app.get('/error', function (req, res) {
-    res.status(status)
+    res.status(global.redirectionError.status)
     res.render('redirection_error_template')
+})
+
+app.get(/(^create)*/, function (req, res) {
+    res.redirect('/error')
 })
 app.listen(process.env.PORT || 8099)
