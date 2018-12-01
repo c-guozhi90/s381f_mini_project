2d447ab6218396bef5671b0da9d5a17c38a7a929
Raven Chan <c.guozhi90@gmail.com>
2018-12-01 16:35:26 +0800

    change homepage - display simple message

---
+++ page_handles/homepage_control.js
@@ -0,0 +1,22 @@
+const http = require('http')
+const Express = require('express')
+
+const Homepage = {
+    homepage: function (req, res) {
+        if (req && res) {
+            var user_name = req.session.user_name
+            var context = []
+            if (!user_name) context.push({ content: 'you have not logged in.' })
+            else context.push({ content: `welcome! ${user_name}` })
+            res.render('homepage_template',
+                {
+                    user_name,
+                    title: 'Welcome',
+                    context,
+
+                })
+        }
+    }
+
+}
+module.exports = Homepage
